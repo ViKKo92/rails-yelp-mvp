190e4ec4d9b3e55f20c99f34e94a793cfafac52d
Viktor Kozma <kozmaviktor1992@gmail.com>
2022-08-17 15:29:57 +0800

    Resto review app built

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+Restaurant.destroy_all
+
+5.times do
+  @restaurant = Restaurant.new(
+    name: Faker::Restaurant.name, 
+    address: Faker::Address.street_address, 
+    category: categories.sample,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  )
+  @restaurant.save
+end
